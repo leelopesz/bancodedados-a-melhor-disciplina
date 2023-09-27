5971783ead1339255604fd979b76af5478ce1aa3
leelopesz <99299548+leelopesz@users.noreply.github.com>
2023-09-26 21:56:36 -0300

    -- Exercício 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -4,5 +4,15 @@ delimiter //
     create procedure sp_ListarAutores()
      select * from autor;
 	//
-    
-    call sp_ListarAutores();
+
+-- Exercício 2
+		
+delimiter //
+  create procedure sp_LivrosPorCategoria(in nome_categoria varchar(200))
+    begin
+	select Livro.Titulo, Autor.Nome, Autor.Sobrenome from Livro
+	inner join Categoria on Livro.Categoria_ID = Categoria.Categoria_ID
+	inner join Autor_Livro on Livro_Livro_ID = Autor_Livro.Livro_ID
+	inner join Autor on Autor_Livro.Autor_Id = Autor.Autor_ID;
+	end//
+    delimiter;
